5af2dab8a907a7f20a3de3537ef57d6f350a645a
Rohit Rasam <rasamrohit@gmail.com>
2025-08-27 18:53:47 +0530

    feat: create and migrate contact model

--- contact/models.py
+++ contact/models.py
@@ -8,15 +8,15 @@ class Contact(models.Model):
         PRIMARY = "primary"
         SECONDARY = "secondary"
 
-    phone_number = models.CharField(max_length=20, null=True)
+    phoneNumber = models.CharField(max_length=20, null=True)
     email = models.EmailField(max_length=100, null=True)
-    linked_id = models.ForeignKey("self", null=True)
-    link_precedence = models.CharField(
+    linkedId = models.ForeignKey("self", null=True, on_delete=models.DO_NOTHING)
+    linkPrecedence = models.CharField(
         max_length=20, choices=Precedence.choices, default=Precedence.PRIMARY
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updateded_at = models.DateTimeField(auto_now_add=True)
-    deleted_at = models.DateTimeField(null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatededAt = models.DateTimeField(auto_now_add=True)
+    deletedAt = models.DateTimeField(null=True)
 
     def __str__(self):
-        return f"{self.email=}, {self.phone_number=}"
+        return f"{self.email=}, {self.phoneNumber=}"
